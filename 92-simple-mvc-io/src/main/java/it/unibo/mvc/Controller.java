994707e0cfcff7029bf01e57467d002aac033fd4
MarcoVincenzi <marco.vincenzi9@studio.unibo.it>
2024-11-19 23:45:43 +0100

    Controller and SimpleGUI

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,38 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.swing.JOptionPane;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String HOME = System.getProperty("user.home");
+    private static final String SEPARATOR = System.getProperty("file.separator");
 
+    private File currentFile = new File(HOME + SEPARATOR + "output.txt");
+
+    public void setFile(final File f){
+        currentFile = f;
+    }
+
+    public File getFile(){
+        return currentFile;
+    }
+
+    public String getPath(){
+        return currentFile.getPath();
+    }
+
+    public void write(final String s){
+        try (PrintStream ps = new PrintStream(currentFile.getPath(), StandardCharsets.UTF_8)) {
+            ps.println(s);
+        } catch (IOException e1) {
+            System.out.println(e1.getMessage());
+        }
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,13 +1,78 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.sound.sampled.Control;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
+    private static final String TITLE = "My first java graphical interface";
+    private final JFrame frame = new JFrame(TITLE);
+    private static final int PROPORTION = 5;
 
-    private final JFrame frame = new JFrame();
+    public void main() {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
+
+    public SimpleGUI (Controller c) {
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JTextArea textArea = new JTextArea();
+        panel.add(textArea,BorderLayout.CENTER);
+        final JButton saveButton = new JButton("Save");
+        panel.add(saveButton,BorderLayout.SOUTH);
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                c.write(textArea.getText());
+            }
+        });
+    }
+
+    private void display() {
+        /*
+         * Make the frame one fifth the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected. In order to deal coherently with multimonitor
+         * setups, other facilities exist (see the Java documentation about this
+         * issue). It is MUCH better than manually specify the size of a window
+         * in pixel: it takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
 
 }
